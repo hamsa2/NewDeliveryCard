b4f8d247df3c0eb28c1b4ac7b9479ee6ba34db61
Nailiya <66hamsa@gmail.com>
2023-01-24 16:25:02 +0700

    init

---
+++ src/test/java/CarddeliverTest.java
@@ -0,0 +1,38 @@
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selectors;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+
+public class CarddeliverTest {
+
+    private String generateDate(int addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+    }
+
+        @Test
+        public void shouldBeSuccessfullyCompleted() {
+            open("http://localhost:9999");
+            $("[data-test-id=city] input").setValue("Анадырь");
+            String currentDate = generateDate(7, "dd.MM.yyyy");
+            $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+            $("[data-test-id=date] input").sendKeys(currentDate);
+            $("[data-test-id=name] input").setValue("Марина Иванова");
+            $("[data-test-id=phone] input").setValue("+79234022252");
+            $("[data-test-id=agreement]").click();
+            $("button.button").click();
+            $(".notification__content")
+                    .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                    .shouldHave(Condition.exactText("Встреча успешно забронирована на " + currentDate));
+
+        }
+    }
+
