2a406f10692093cd9548e585326dea2a0469e4a6
Nailiya <66hamsa@gmail.com>
2023-01-24 21:51:48 +0700

    init

--- src/test/java/CarddeliverTest.java
+++ src/test/java/CarddeliverTest.java
@@ -18,21 +18,20 @@ public class CarddeliverTest {
         return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
     }
 
-        @Test
-        public void shouldBeSuccessfullyCompleted() {
-            open("http://localhost:9999");
-            $("[data-test-id=city] input").setValue("Анадырь");
-            String currentDate = generateDate(7, "dd.MM.yyyy");
-            $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
-            $("[data-test-id=date] input").sendKeys(currentDate);
-            $("[data-test-id=name] input").setValue("Марина Иванова");
-            $("[data-test-id=phone] input").setValue("+79234022252");
-            $("[data-test-id=agreement]").click();
-            $("button.button").click();
-            $(".notification__content")
-                    .shouldBe(Condition.visible, Duration.ofSeconds(15))
-                    .shouldHave(Condition.exactText("Встреча успешно забронирована на " + currentDate));
-
-        }
+    @Test
+    public void shouldBeSuccessfullyCompleted() {
+        open("http://localhost:9999");
+        $("[data-test-id=city] input").setValue("Анадырь");
+        String currentDate = generateDate(7, "dd.MM.yyyy");
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id=date] input").sendKeys(currentDate);
+        $("[data-test-id=name] input").setValue("Марина Иванова");
+        $("[data-test-id=phone] input").setValue("+79234022252");
+        $("[data-test-id=agreement]").click();
+        $("button.button").click();
+        $(".notification__content")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + currentDate));
     }
+}
 
